49a7df58d6e39bfef1cafa6ffd67678b20f66aa5
bufik <bufik@newFounders>
2018-09-23 20:52:50 +0200

    Added a lot of documentation in the classes. Removed one obsolete colouring statement in the playfield class before drawing the bat.

--- HelloProcessing/src/test/janw/nl/Ball.java
+++ HelloProcessing/src/test/janw/nl/Ball.java
@@ -3,30 +3,51 @@ package test.janw.nl;
 // import processing.core.PApplet; // Not necessary? 
 
 public class Ball { 
+
+	// This class was written as a part of creating a squash program for the Open.HPI.de course "Java Capstone series 1."
+	// This class is responsible for the ball; it has attributes for x- & y-position, the speed and for convenience its speeds 
+	// along the x-axis and y-axis (using sin/cos of the angle and the speed)
+	// This class has methods for creating, moving and drawing of the ball
+	// It has private methods for setting the speed, and bouncing against the walls, ceiling or the bat. 
+	
+	// What I have doubts about, is the fact that the ball is aware of the canvas (subclass of PApplet of the processing framework). 
+	// I would like it if the ball was loosely coupled, without a hard reference. 
+	
+	// ToDo: For now, if you miss the ball there will only be a System.out.println()-message. That should be made nicer. 
+	// My current idea is to raise either an exception; or to raise an event, so I will learn something about catching events. 
+	// Furthermore, I think it would be nice to refactor the whole solution to a model-view-controller. 
+	// However, as the deadline of the course approaches, and the rest of the team does not seem to be very active, 
+	// this will most likely be the solution to be submitted.
 	
 	private float x_as ; 
 	private float y_as ; 
 	private float radius; 
 	private PlayField playField;
-	// private boolean goingUp = true;
 	private double speed; 
 	private double x_speed;
 	private double y_speed; 
 	private int angle; 
 	
-	// constructor
+
 	public Ball( PlayField playField, float start_x, float start_y, float radius, double speed, int angle ) {
+		// constructor
+		// The constructor takes the canvas as input, so it knows where it lives (and can ask the size of its canvas (eg. ceiling and borders)
+		// Furthermore it gets its starting position (start_x, start_y) and its starting speed and starting angle). 
+		// Finally it is being told its size in radius.
 		this.playField = playField; 
 		this.x_as = start_x ;
 		this.y_as = start_y ;
 		this.radius = radius;
 		this.angle = angle;
-		setSpeeds( speed );  // Adust all speeds, total velocity, and x-axis speed, and y-axis speed
+		setSpeeds( speed );  // Adjust all speeds, total velocity, and x-axis speed, and y-axis speed
 	
 	}
 	
 	public void move() {
-		
+		// Method move: This method moves the ball. Furthermore, it checks if the ball hits the ceiling, one of the walls, or the bat/paddle. 
+		// If it is at the same height as the bat/paddle, but doesn't hit it for now it generates a message that you lose, currently as a System.out.println()-message.
+		// Furthermore, as a temporarily solution, the ground reacts as a boundary as well, so the ball will never leave the canvas. 
+
 		// System.out.println("speed " + speed + " x_as " + x_as + " x_speed " + x_speed  + " y_as " + y_as + " y_speed " + y_speed + " batyaxis " + playField.getBat().getY_as()  ); 
 		
 		// Check if it hits the right wall
@@ -57,38 +78,45 @@ public class Ball {
 				// Yes it hits the bat
 				bounceHorizontal( );
 			} else {
-				//GAME OVER 
+				// It misses the bat: GAME OVER 
 				// ToDo Implement nice ending of the game
 				System.out.println("Game over: speed & y_as " + speed + " " + y_as ); 
-				//For now, bounce at the bottom
+				// For now, bounce at the bottom
 				if ( y_as >= playField.getPlayfieldLength() ) {
 					bounceHorizontal( );
 				}
 			}
 		} 
 		
+		// Adjust the position of the ball.
 		y_as += y_speed; 
 		x_as += x_speed;
 		
 	}
 	
+
 	public void draw() {
+		// Paint the ball on the canvas.
 		playField.fill(200,0,0); // Color! 
     	playField.ellipse(x_as, y_as, radius, radius);
     	// x_as is the horizontal place, then  the vertical place , length & height of rectangle, rounded corners.
 	}
 	
+
 	private void bounceHorizontal() {
-		//System.out.println("bounceHorizontal");
+		// Bounce on a horizontal boundary (eg. ceiling or bat/paddle)
+		// System.out.println("bounceHorizontal");
 		this.y_speed = this.y_speed * (-1);
 	}
 	
 	private void bounceVertical() {
+		// Bounce on a vertical boundary (eg. one of the walls)
 		//System.out.println("bounceVrtical");
 		this.x_speed = this.x_speed * (-1);
 	}
 	
 	private void setSpeeds(double newSpeed ) {
+		// Set the speed; total speed and the relative speed on the axis of the canvas; using the sin and cos of the angle.
 		this.speed = newSpeed;
 		this.x_speed = java.lang.Math.sin(java.lang.Math.toRadians(angle)) * speed;
 		this.y_speed = java.lang.Math.cos(java.lang.Math.toRadians(angle)) * speed;
--- HelloProcessing/src/test/janw/nl/Bat.java
+++ HelloProcessing/src/test/janw/nl/Bat.java
@@ -1,60 +1,79 @@
 package test.janw.nl;
 
 public class Bat { 
+	// This class was written as a part of creating a squash program for the Open.HPI.de course "Java Capstone series 1."
 	
+	// This class is responsible for the bat; it has attributes for x- & y-position and the speed 
+	// It has a fixed length and height; it would be nice to get those as parameters in the constructore in a next iteration.
+	// It also remembers on which canvas (playing field) it is painted.
 	private float x_as; 
 	private float y_as;  
+	private float speed = 3; 
 	private float batLength = 70;
 	private float batHeight = 14; 
 	private PlayField playField;
 	
-	// constructor
+	
 	public Bat( PlayField playField ) {
+		// Constructor
+		// The constructor takes the canvas as input, so it knows where it lives 
+		// and can ask the size of its canvas (borders)
 		this.playField = playField; // tell the bat where he must draw himself
-		this.y_as = this.playField.getPlayfieldLength() - 40; // leave some space under the bat.
+		this.y_as = this.playField.getPlayfieldLength() - 40; // leave some space under the bat; would be nice to make this an attribute or parameter
 		this.x_as = ( this.playField.getPlayfieldLength() - this.batLength ) /2; // put the bat in the middle of the playfield
 	}
 	
 	public void moveLeft() {
-		if ( x_as - 3 < 0 ) {
+		// Move the bat to the left; taking into account the speed of the bat; ánd the wall
+		if ( x_as - speed < 0 ) {
 			x_as = 0; 
 		} else {
-			x_as -= 3; 
+			x_as -= speed; 
 		}
 	}
 	
 	public void moveRight() {
-		if ( x_as + 3 >  ( playField.getPlayfieldLength() - batLength) ) {
+		// Move the bat to the right; taking into account the speed of the bat; ánd the wall
+		if ( x_as + speed >  ( playField.getPlayfieldLength() - batLength) ) {
 			x_as = playField.getPlayfieldLength() - ( batLength + 1 ) ; 
 		} else {
-			x_as += 3; 
+			x_as += speed; 
 		};
 	}
 	
 	
 	public void draw() {
+		// Draw the bat as a rectangle on the canvas
 		playField.fill(20,50,240); // give some color
     	playField.rect(x_as, y_as , batLength, batHeight, 4);
     	// x_as is the horizontal place, then  the vertical place , length & height of rectangle, rounded corners.
 	}
 	
-	// Getters & setters	
+
+	
+	
 	public float getX_as() {
+		// Generated Getters & setters for the attributes
 		return x_as;
 	}
 	public void setX_as(float x_as) {
+		// Generated Getters & setters for the attributes
 		this.x_as = x_as;
 	}
 	public float getY_as() {
+		// Generated Getters & setters for the attributes
 		return y_as;
 	}
 	public float getBatLength() {
+		// Generated Getters & setters for the attributes
 		return batLength;
 	}
 	public void setBatlength(float batLength) {
+		// Generated Getters & setters for the attributes
 		this.batLength = batLength;
 	}
 	public float getBatHeight() {
+		// Generated Getters & setters for the attributes
 		return batHeight;
 	}
 
